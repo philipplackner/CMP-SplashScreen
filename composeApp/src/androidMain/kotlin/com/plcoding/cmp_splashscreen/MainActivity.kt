d79c28a245487645f8e1434d8e1e3af06869d7b1
Philipp Lackner <mail@pl-coding.com>
2024-07-15 19:29:06 +0200

    Initial commit

---
+++ composeApp/src/androidMain/kotlin/com/plcoding/cmp_splashscreen/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.plcoding.cmp_splashscreen
+
+import App
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            App()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppAndroidPreview() {
+    App()
+}
